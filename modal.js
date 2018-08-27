cadff4f6b40d549c6284f0c900e32ad6230dd5ab
GoutamKulabhi <goutam.kulabhi@gmail.com>
2018-08-27 20:00:44 +0530

    initial-commit

---
+++ modal.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+
+	
+
+	$(".color1").click(function(){
+		$(".container").css("display","block");
+	});
+
+	$(".color2").click(function(){
+		$(".container").css("display","none");
+	});
+
+});
